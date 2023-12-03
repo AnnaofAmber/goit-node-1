067a06d2c1ae428fb79c0faabfbe134d44d322db
Anna Boichuk <anna.boichuk.o@gmail.com>
2023-12-03 19:21:30 +0200

    step 4-5

--- contacts.js
+++ contacts.js
@@ -1,6 +1,5 @@
 const fs = require('fs').promises;
 const path = require('path');
-const { nanoid } = require("nanoid");
 const contactsPath = path.join(__dirname, "db/contacts.json");
  
 
@@ -16,20 +15,25 @@ return contact || null
   }
   
   async function removeContact(contactId) {
-
     const contacts = await listContacts();
     const contactIndex = contacts.findIndex((contact) => contact.id === contactId);
-    // if (contactIndex === -1) return null;
+    if (contactIndex === -1) return null;
     const [deletedContact] = contacts.splice(contactIndex, 1);
     await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-    return result || null;
-
+    return deletedContact;
   }
   
   async function addContact(name, email, phone) {
     const contacts = await listContacts();
-    const newContact = { id: nanoid(), name, email, phone };
+    const newContact = { id: Math.floor(Math.random() * 10000000001).toString(), name, email, phone };
     contacts.push(newContact);
     await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
     return newContact;
   }
+
+  module.exports = {
+    getContactById,
+    listContacts,
+    removeContact,
+    addContact,
+  };
